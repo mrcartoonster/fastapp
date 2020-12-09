faa267208b8ec39ff701f550f53c0efd215db0d4
Evan Baird <mrcartoonster@gmail.com>
2020-12-09 12:15:13 -0700

    📦 NEW: BEER-29 #time 30m #comment Creating password endpoint

--- tests/test_people.py
+++ tests/test_people.py
@@ -20,3 +20,33 @@ def test_persons():
 
     # THEN a 200 is returned
     assert resp.status_code == 200
+
+
+def test_person_num():
+    """
+    Ensure when number is passed to query, the correct number of persons
+    is returned.
+    """
+    # WHEN FastAPI GET request is given
+
+    # GIVEN GET request to person endpoint with query of 28
+    resp = client.get("/person", params={"num": 28})
+
+    # THEN 28 persons are returned
+    resp_dict = resp.json()
+
+    assert len(resp_dict) == 28
+
+
+def test_person_num_failing():
+    """
+    Ensure when more than 50 persons are requested, 422 is returned and
+    no persons are returned.
+    """
+    # WHEN FastAPI GET request is given
+
+    # GIVEN GET request to person endpoint with query of 51
+    resp = client.get("/person", params={"num": 51})
+
+    # THEN 422 is returned.
+    assert resp.status_code == 422
