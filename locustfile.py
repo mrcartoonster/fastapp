4c3cdde978080a977170e3060aec3aee8d11811f
Evan Baird <mrcartoonster@gmail.com>
2020-12-09 11:07:07 -0700

    📦 NEW: BEER-29 #comment Adding additional libraries #in

--- locustfile.py
+++ locustfile.py
@@ -1,32 +1,5 @@
 # -*- coding: utf-8 -*-
-import random
-
-from locust import HttpUser, between, tag, task
-
-
-class WebsiteUser(HttpUser):
-    wait_time = between(1, 1.5)
-
-    def on_start(self):
-        self.uid = str(random.randint(0, 100_000)).zfill(6)
-
-    @tag("attempt", "ligth")
-    @task(2)
-    def attempt(self):
-        self.client.get("/hello/")
-
-    @tag("simulate", "light")
-    @task(1)
-    def simulate(self):
-        self.client.post(
-            "/simulate/",
-            json={
-                "uid": self.uid,
-                "n_sim": random.randint(10, 20),
-            },
-        )
-
-    @tag("sleep", "heavy")
-    @task(2)
-    def slepper(self):
-        self.client.get("/sleep/")
+"""
+Locust file for stress testing.
+"""
+# -*- coding: utf-8 -*-
