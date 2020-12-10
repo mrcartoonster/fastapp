2bd5d410bb4581ec4a7ca5c72bf5887b56922a6a
Evan Baird <mrcartoonster@gmail.com>
2020-12-10 16:16:54 -0700

    📦 NEW: BEER-29 moving to swagger instead of redoc

--- description.py
+++ description.py
@@ -4,9 +4,8 @@ Description for FastAPI redoc/docs screen.
 """
 desc = """
 Simple [FastAPI](fastapi.tiangolo.com/) Digital Ocean [App Platform](https://do.co/3gBHLOn) Deploy example.
-Go heare to check out the [ReDoc](https://mrcartoonsters.tk/redoc)
+Don't forget to view the [ReDoc](https://mrcartoonsters.tk/redoc)
 
 ![GitHub Repo stars](https://img.shields.io/github/stars/mrcartoonster/fastapp?style=social)
 
-<iframe src="https://github.com/sponsors/mrcartoonster/button" title="Sponsor mrcartoonster" height="35" width="116" style="border: 0;"></iframe>
 """
