60d87488f5a2d741dabc7932740d7008d19eae80
Evan Baird <mrcartoonster@gmail.com>
2020-12-11 06:05:26 -0700

    📦 NEW: Removing tags

--- description.py
+++ description.py
@@ -5,8 +5,4 @@ Description for FastAPI redoc/docs screen.
 desc = """
 Simple [FastAPI](fastapi.tiangolo.com/) Digital Ocean [App Platform](https://do.co/3gBHLOn) Deploy example.
 Don't forget to view the [ReDoc](https://mrcartoonsters.tk/redoc)
-
-![GitHub Repo stars](https://img.shields.io/github/stars/mrcartoonster/fastapp?style=social)
-
-![GitHub issues](https://img.shields.io/github/issues/mrcartoonster/fastapp)
 """
