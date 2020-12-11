ae84d8b4b059c538c6223243fdd2e4ddaeaae0d4
Evan Baird <mrcartoonster@gmail.com>
2020-12-10 22:09:26 -0700

    📦 NEW: Final push

--- description.py
+++ description.py
@@ -8,4 +8,5 @@ Don't forget to view the [ReDoc](https://mrcartoonsters.tk/redoc)
 
 ![GitHub Repo stars](https://img.shields.io/github/stars/mrcartoonster/fastapp?style=social)
 
+![GitHub issues](https://img.shields.io/github/issues/mrcartoonster/fastapp)
 """
